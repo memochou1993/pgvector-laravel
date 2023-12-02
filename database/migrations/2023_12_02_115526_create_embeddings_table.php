4efad0d8564bb97d3635df85299f602b9b562fdd
William Chou <williamchou@kklab.com>
2023-12-02 20:32:11 +0800

    Rename index

--- database/migrations/2023_12_02_115526_create_embeddings_table.php
+++ database/migrations/2023_12_02_115526_create_embeddings_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
 
         // This is a Postgres-specific index that allows us to do fast nearest-neighbor searches
         // when there are a lot of high-dimensional embeddings in the database.
-        DB::statement('CREATE INDEX my_index ON embeddings USING ivfflat (embedding vector_l2_ops) WITH (lists = 100)');
+        DB::statement('CREATE INDEX embedding_index ON embeddings USING ivfflat (embedding vector_l2_ops) WITH (lists = 100)');
     }
 
     /**
